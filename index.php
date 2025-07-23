b444c0a1e4b9f3c3f2ef199b0e27bc7d045e559f
rizkyhansen <rizkyhansenn@gmail.com>
2025-07-23 16:55:15 +0700

    menambahkan tet lagi

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis repellendus enim minus dicta alias aliquid eius debitis similique, quod eveniet autem nisi consequatur dolorem optio soluta blanditiis dignissimos distinctio iusto.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum suscipit dolorum repellat recusandae asperiores cumque fugit qui illum. Recusandae, doloremque laboriosam? Qui, sint laboriosam at impedit amet itaque magnam vel?</p>
 </body>
 </html>
